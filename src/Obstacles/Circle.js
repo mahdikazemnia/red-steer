71cb81956643ed7008af5a39980e44bf4007ec6c
Mehdi Kazemnia <mehdikazemnia@users.noreply.github.com>
2018-07-18 16:10:58 +0430

    circlular obstacle basics

---
+++ src/Obstacles/Circle.js
@@ -0,0 +1,27 @@
+const V2D = require('vectors-2d');
+
+class Circle {
+
+    constructor(settings, chain) {
+
+        // join the chain
+        this.info = {};
+        this.info.id = chain.obstacles.length;
+        this.info.type = 'Circle';
+        this.chain = chain;
+        this.chain.obstacles.push(this.info);
+
+        // position
+        this.position = new V2D(settings.position);
+
+        // radius
+        this.radius = settings.radius;
+
+        // attach referencable share info (objects)
+        this.info.position = this.position;
+
+    }
+
+}
+
+module.exports = Circle;
