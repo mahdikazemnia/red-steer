2f78a514f71a60fbed43e62e075ff373b680d3d6
Mehdi Kazemnia <mehdikazemnia@users.noreply.github.com>
2018-07-18 15:43:08 +0430

    mover basics

---
+++ src/Mover.js
@@ -0,0 +1,20 @@
+const V2D = require('vectors-2d');
+
+class Mover {
+
+    constructor(settings, chain) {
+
+        // info object to share with the chain
+        this.info = {};
+        this.info.id = chain.movers.length;
+
+        // join the chain
+        this.chain = chain;
+        this.chain.movers.push(this.info);
+
+    }
+
+
+}
+
+module.exports = Mover;
